9b92008ca7624e353cc9bec0938397a98bca2468
Matthew Roseman <mrr77@case.edu>
2016-10-08 13:51:15 -0400

    got android camera to show full image and added score and upload buttons
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- GoCaptureApp/app/src/main/java/com/stohio/gocaptureapp/MainActivity.java
+++ GoCaptureApp/app/src/main/java/com/stohio/gocaptureapp/MainActivity.java
@@ -1,32 +1,54 @@
 package com.stohio.gocaptureapp;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.content.Context;
+import android.os.Environment;
+import android.provider.MediaStore.Images.Media;
+import android.Manifest;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1, b2;
+    Button b1;
     ImageView iv;
 
-    String mCurrentPhotoPath;
-
     private static final String TAG = "OCVSample::Activity";
+
+    private static final int TAKE_PHOTO_CODE = 1;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        verifyStoragePermissions(this);
         setContentView(R.layout.activity_main);
 
         b1 = (Button)findViewById(R.id.button);
@@ -35,37 +57,105 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                takePhoto();
             }
         });
     }
 
-    private File createImageFile() throws IOException {
-        // Create Image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
+    private void updateButtons() {
+        // delete b1
+        RelativeLayout main_layout = (RelativeLayout)findViewById(R.id.activity_main);
+        // add two buttons, score and upload
+        LinearLayout buttons = new LinearLayout(this);
+        LayoutParams params = new LinearLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+        buttons.setLayoutParams(params);
+        buttons.setOrientation(LinearLayout.HORIZONTAL);
 
-        //  Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
+        Button score = new Button(this);
+        score.setText("Score");
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreGame();
+            }
+
+        });
+        Button upload = new Button(this);
+        upload.setText("Upload");
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadGame();
+            }
+
+        });
+
+        main_layout.addView(buttons);
+        buttons.addView(score);
+        buttons.addView(upload);
+    }
+
+    private void scoreGame() {
+        // create json from image and send to server @score game url
+    }
+
+    private void uploadGame() {
+        //  create json from image and send to server @upload game url
+    }
+
+    private void takePhoto(){
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getTempFile(this)) );
+        startActivityForResult(intent, TAKE_PHOTO_CODE);
+    }
+
+    private File getTempFile(Context context){
+        //it will return /sdcard/image.tmp
+        final File path = new File( Environment.getExternalStorageDirectory(), context.getPackageName() );
+        if(!path.exists()){
+            path.mkdir();
+        }
+        return new File(path, "tmp.jpg");
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
-        iv.setImageBitmap(bp);
+        if (resultCode == RESULT_OK) {
+            switch(requestCode){
+                case TAKE_PHOTO_CODE:
+                    final File file = getTempFile(this);
+                    try {
+                        Bitmap captureBmp = Media.getBitmap(getContentResolver(), Uri.fromFile(file) );
+                        iv.setImageBitmap(captureBmp);
+                        iv.invalidate();
+                        updateButtons();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
